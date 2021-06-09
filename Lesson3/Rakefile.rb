ef299cfb2e2d836525ee3e72b990005ed0656ee8
AgentSmith <vip1313@gmail.com>
2021-06-09 16:42:07 +0300

    dobavil vremya

--- Lesson3/Rakefile.rb
+++ Lesson3/Rakefile.rb
@@ -1,4 +1,7 @@
+desc 'задача по умолчанию'
 task default: :timedate
+
+desc 'вывод текущей даты и времени'
 task :timedate do
-	puts Time.new.strftime(" %d-%B-%Y")  
+	puts Time.new.strftime(" %d-%B-%Y  %H:%M:%H")  
 end
