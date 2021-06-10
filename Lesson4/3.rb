d6e1992910fad24bf0d38f20cc4b701dc52b1c3c
AgentSmith <vip1313@gmail.com>
2021-06-11 02:28:09 +0300

    create massiv dney nedeli

---
+++ Lesson4/3.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+colors = {
+  red: 'красный',
+  orange: 'оранжевый',
+  yellow: 'желтый',
+  green: 'зеленый',
+  blue: 'синий',
+  indigo: 'индиго',
+  violet: 'фиолетовый'
+}
+puts colors [:green]
