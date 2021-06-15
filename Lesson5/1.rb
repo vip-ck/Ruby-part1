82552abf318748160bed158e83a5f3d834b89c74
AgentSmith <vip1313@gmail.com>
2021-06-16 01:23:03 +0300

    create 1.rb

---
+++ Lesson5/1.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class User
+  def s_fio(fam, imy, otch)
+    @fam = fam
+    @imy = imy
+    @otch = otch
+  end
+
+  def get_fam
+    @fam
+  end
+
+  def get_imy
+    @imy
+  end
+
+  def get_otch
+    @otch
+  end
+end
+student = User.new
+teacher = User.new
+student.s_fio('Заика', 'Виталий', 'Александрович')
+teacher.s_fio('бал', 'блам', 'патам')
+puts "Имя студента: #{student.get_fam} #{student.get_imy} #{student.get_otch}"
+puts "Имя Преподователя: #{teacher.get_fam} #{teacher.get_imy} #{teacher.get_otch}"
