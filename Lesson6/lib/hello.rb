8017d701624dce2e02decfcf56121bcc48f4882d
AgentSmith <vip1313@gmail.com>
2021-06-21 16:54:23 +0300

    vnesli izmeneniya

---
+++ Lesson6/lib/hello.rb
@@ -0,0 +1,16 @@
+class Hello
+		def initialize(priv)
+			@priv = priv
+		end
+		def set_hello
+			if @priv >= 6 && @priv < 12
+				'доброе утро'
+				elsif @priv >= 12 && @priv < 18
+				'Добрый день'
+			elsif @priv >= 18 && @priv <= 23
+				'добрый вечер'
+			else 'доброй ночи'
+		
+end
+end
+end
---
+++ Lesson6/main.rb
@@ -0,0 +1,10 @@
+	
+	require './lib/hello'
+
+		timemoment = Hello.new(Time.new.hour)
+		#puts Time.new.hour
+		puts timemoment.set_hello
+		
+
+
+
